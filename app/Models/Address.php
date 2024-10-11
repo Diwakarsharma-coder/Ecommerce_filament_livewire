036ea5f586554da7e06d7fec9b3f9622e96af261
jamtechba@gmail.com <jamtechba@gmail.com>
2024-10-11 19:02:30 +0530

    Upload

---
+++ app/Models/Address.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'order_id', 'first_name', 'last_name', 'street_address', 'city', 'state', 'country', 'zip_code', 'phone'];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFullnameAttribute(): string
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+}
